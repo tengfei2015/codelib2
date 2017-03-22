f5fa9aa8048cdeadc7534942ead715c422cd4917
tengfei2015 <xitengfei@aragoncs.com>
2017-03-22 13:43:40 +0800

    update

---
+++ sql/multisite3_local_update.sql
@@ -0,0 +1,14 @@
+update wp_site set domain="local.multisite3.com" where id=1;
+
+#wp_options
+update wp_options set option_value = 'http://local.multisite3.com/' where option_id=1;
+update wp_options set option_value = 'http://local.multisite3.com/' where option_id=2;
+update wp_40_options set option_value = 'http://nocturnal2017.local.multisite3.com/' where option_id=1;
+update wp_40_options set option_value = 'http://nocturnal2017.local.multisite3.com/' where option_id=2;
+update wp_41_options set option_value = 'http://mexico2018.local.multisite3.com/' where option_id=1;
+update wp_41_options set option_value = 'http://mexico2018.local.multisite3.com/' where option_id=2;
+
+#wp_blogs
+update wp_blogs set domain='local.multisite3.com' where blog_id=1;
+update wp_blogs set domain='nocturnal2017.local.multisite3.com' where blog_id=40;
+update wp_blogs set domain='mexico2018.local.multisite3.com' where blog_id=41;
