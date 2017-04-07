9c774c126968a6eb68abe592cd6657b1c89684ca
tengfei2015 <xitengfei@aragoncs.com>
2017-04-07 19:23:04 +0800

    update orlando2018

--- sql/multisite3.sql
+++ sql/multisite3.sql
@@ -8,9 +8,11 @@ update wp_40_options set option_value = 'http://nocturnal2017.local.multisite3.c
 update wp_40_options set option_value = 'http://nocturnal2017.local.multisite3.com/' where option_id=2;
 update wp_41_options set option_value = 'http://mexico2018.local.multisite3.com/' where option_id=1;
 update wp_41_options set option_value = 'http://mexico2018.local.multisite3.com/' where option_id=2;
+update wp_42_options set option_value = 'http://orlando2018.local.multisite3.com/' where option_id=1;
+update wp_42_options set option_value = 'http://orlando2018.local.multisite3.com/' where option_id=2;
 
 #wp_blogs
 update wp_blogs set domain='local.multisite3.com' where blog_id=1;
 update wp_blogs set domain='nocturnal2017.local.multisite3.com' where blog_id=40;
 update wp_blogs set domain='mexico2018.local.multisite3.com' where blog_id=41;
-
+update wp_blogs set domain='orlando2018.local.multisite3.com' where blog_id=42;
