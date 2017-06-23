fd54a8bc86eb62f680403ec7b273a60501fff4e8
tengfei2015 <xitengfei@aragoncs.com>
2017-06-23 18:40:06 +0800

    update

--- sql/multisite3_local_update.sql
+++ sql/multisite3_local_update.sql
@@ -13,6 +13,8 @@ update wp_43_options set option_value = 'http://project-z.local.multisite3.com/'
 update wp_43_options set option_value = 'http://project-z.local.multisite3.com/' where option_id=2;
 update wp_44_options set option_value = 'http://audiotistic.local.multisite3.com/' where option_id=1;
 update wp_44_options set option_value = 'http://audiotistic.local.multisite3.com/' where option_id=2;
+update wp_45_options set option_value = 'http://escape2017.local.multisite3.com/' where option_id=1;
+update wp_45_options set option_value = 'http://escape2017.local.multisite3.com/' where option_id=2;
 
 #wp_blogs
 update wp_blogs set domain='local.multisite3.com' where blog_id=1;
@@ -21,3 +23,4 @@ update wp_blogs set domain='mexico2018.local.multisite3.com' where blog_id=41;
 update wp_blogs set domain='orlando2017.local.multisite3.com' where blog_id=42;
 update wp_blogs set domain='project-z.local.multisite3.com' where blog_id=43;
 update wp_blogs set domain='audiotistic.local.multisite3.com' where blog_id=44;
+update wp_blogs set domain='escape2017.local.multisite3.com' where blog_id=45;
