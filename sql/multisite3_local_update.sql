d92656707a865cfb051a4a3c2cc26bdfbcf096c8
tengfei2015 <xitengfei@aragoncs.com>
2017-07-19 22:55:26 +0800

    update

--- sql/multisite3_local_update.sql
+++ sql/multisite3_local_update.sql
@@ -15,6 +15,10 @@ update wp_44_options set option_value = 'http://audiotistic.local.multisite3.com
 update wp_44_options set option_value = 'http://audiotistic.local.multisite3.com/' where option_id=2;
 update wp_45_options set option_value = 'http://escape2017.local.multisite3.com/' where option_id=1;
 update wp_45_options set option_value = 'http://escape2017.local.multisite3.com/' where option_id=2;
+update wp_46_options set option_value = 'http://dreamstatesocal2017.local.multisite3.com/' where option_id=1;
+update wp_46_options set option_value = 'http://dreamstatesocal2017.local.multisite3.com/' where option_id=2;
+update wp_47_options set option_value = 'http://japan2018.local.multisite3.com/' where option_id=1;
+update wp_47_options set option_value = 'http://japan2018.local.multisite3.com/' where option_id=2;
 
 #wp_blogs
 update wp_blogs set domain='local.multisite3.com' where blog_id=1;
@@ -24,3 +28,5 @@ update wp_blogs set domain='orlando2017.local.multisite3.com' where blog_id=42;
 update wp_blogs set domain='project-z.local.multisite3.com' where blog_id=43;
 update wp_blogs set domain='audiotistic.local.multisite3.com' where blog_id=44;
 update wp_blogs set domain='escape2017.local.multisite3.com' where blog_id=45;
+update wp_blogs set domain='dreamstatesocal2017.local.multisite3.com' where blog_id=46;
+update wp_blogs set domain='japan2018.local.multisite3.com' where blog_id=47;
